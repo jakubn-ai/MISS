776402baa92197058e782b3a3d9d74233528e574
drewko <bdrzewicki@interia.pl>
2021-03-19 19:30:53 +0100

    added traffic simulation

--- project/main.py
+++ project/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     env = simpy.rt.RealtimeEnvironment()
     graph = Graph()
 
-    vehicle = Vehicle(env, 1, graph.start_node, graph.end_node, graph)
-    vehicle2 = Vehicle(env, 2, graph.start_node, graph.end_node, graph)
+    vehicle = Vehicle(env, 1, graph.start_node, graph.end_node, graph, 10)
+    vehicle2 = Vehicle(env, 2, graph.start_node, graph.end_node, graph, 20)
 
     env.run(until=120)
